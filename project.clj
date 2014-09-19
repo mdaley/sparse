44f96c4e49763a83b5b98de1fcc17eb1b94fac0c
Matt Daley <matt.daley@blueyonder.co.uk>
2014-09-19 18:43:16 +0100

    Only docs on this branch.

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject sparse "0.1.0-SNAPSHOT"
-  :description "Generate sparse bit array representations of fundamental data types."
-  :url "http://github.com/mdaley/sparse"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
