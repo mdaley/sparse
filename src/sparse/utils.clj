36edb766ef3b0f415167a8b432ccdbaa269d4da1
Matthew Daley <matthew.daley@nokia.com>
2014-10-13 14:37:58 +0100

    Moving to using more compact bitstrings instead of sequences.

--- src/sparse/utils.clj
+++ src/sparse/utils.clj
@@ -1,24 +1,21 @@
 (ns sparse.utils)
 
 (defn bit-to-set
-  "Returns the bit to set in an array of bits of length size which represents
-   a value's position in the range (inclusive of zero).
+  "Returns the bit to set in an array of bits of length `size` which represents
+   the value `val`'s position in the range zero to `range`.
 
    For example if the number of bits is 10 and the range is 99, the value
-   0 has the bit position 0, hence the bit array would be 0000000001.
+   0 has the bit position 0, representing the bit array 0000000001.
 
-   If the value is 99 the bit position is 10, hence the bit array is 1000000000.
+   If the value is 99 the bit position is 9, representing 1000000000.
 
-   If the value is 33, the bit position is 4, hence the bit array is 0000001000.
+   If the value is 33, the bit position is 3, representing 0000001000..
 
-   Don't forget that several numbers, along with zero, can be represented by
-   bit position 0.
-
-   Of course if the number range is large and the bit array is small, then many
+   Of course if the number range is large and the bit array is small, many
    numbers will be represented by each bit position. If the number range is the
    same as the number of bits, e.g. the number of bits is 100 and the number range
-   is (zero to) 99, then one bit represents each number - bit 14 represents the
-   number 13, and bit 1 represents the number zero.
+   is (zero to) 99, then one bit represents each number - bit 13 represents the
+   number 13, and bit 0 represents the number zero.
 
    If the number range is less than the number of bits, the value is arrived
    at by multiplying range and value by size / range, making the range the same as
@@ -26,35 +23,70 @@
    range.
 
    Throws AssertionError if value is outside the range, or value < 0, or range <= 0"
-  [^long size ^long val ^long range]
+  [^long size val range]
   (assert (<= val range) "Value must be <= the range.")
   (assert (>= val 0) "Value must be >= zero.")
   (assert (pos? range) "Range must be > zero.")
   (if (>= range size)
-    (let [result (->
-                  (/ size range)
-                  (* val)
-                  (inc)
+    (let [max-bit (dec size)
+          result (->
+                  (/ val range)
+                  (* size)
                   (Math/floor)
                   (int))]
-      (if (> result size)
-        size
+      (if (> result max-bit) ; at limit result will be too high.
+        max-bit
         result))
     (bit-to-set size (* val (/ size range)) size)))
 
+(defn bit-pos->num
+  "Turn the position of a bit in a particular size sequence into the original value, or
+   more precisely into three values: the highest possible value that could be represented
+  by this bit position, the mid-value and the lowest possible value.
+
+  Suppose that an 8 bit sparse array represents a value in the range zero to 100,000.
+  Fairly obviously, one bit represents a large range of values. In this case, the
+  sparse sequence (1 0 0 0 0 0 0 0) can have been generated from numbers from 100,000
+  down to 87,500 and the mid-point value is 93,750.
+
+  So, sparse arrays that a small relative to the number that they encode and rather
+  imprecise. However, sparse arrays that are large relative to the number that they encode
+  can be much more precise."
+  [^long size ^long pos ^long range]
+  (let [top (int (Math/floor (/ (* range pos) size)))
+        bottom (int (Math/ceil (/ (* range (dec pos)) size)))
+        mid (int (Math/floor (/ (+ top bottom) 2)))]
+    [top mid bottom]))
+
 (defn zero-seq
- "Build a sequence of n zeros"
  [n]
  (repeat n 0))
+
+(defn index-of-first-one
+  [bit-seq]
+  (first (keep-indexed #(when (= 1 %2) %1) bit-seq)))
+
+(defn set-bit-pos
+  [bit-seq]
+  (- (count bit-seq) (index-of-first-one bit-seq)))
 
 (defn num->single-bit-in-seq
   "Returns an array of bits of size length with a single bit set that represents
    the position of the value in the range (inclusive of zero)."
-  [^Integer size ^Number val ^Number range]
+  [^long size ^long val ^long range]
   (let [bit-to-set (bit-to-set size val range)
         starting-zeros-count (- size bit-to-set)
         ending-zeros-count (dec bit-to-set)]
     (flatten (conj (zero-seq ending-zeros-count) '(1) (zero-seq starting-zeros-count)))))
+
+(defn bits->binarystring
+  [size bits]
+  (reduce (fn [s x] (str s (if (contains? bits x) "1" "0"))) "" (reverse (range size))))
+
+(defn single-bit-in-seq->num
+  "Turns a bit sequence with a single bit set back into a value within the specified range"
+  [bit-seq ^long range]
+  (second (bit-pos->num (count bit-seq) (set-bit-pos bit-seq) range)))
 
 (defn long->bit-seq
   ([^long l]
@@ -65,6 +97,19 @@
        (assert (>= prefix-bit-count 0) "Number of bits must be enough to contain the value.")
        (flatten (conj bit-seq
                       (take prefix-bit-count (repeat 0)))))))
+
+(defn long->bitstring
+  "Turn a long value 'l' into a string of bits. In the second form, the number of bits 'b'
+  can be specified with zero padding occurring where necessary. The number of bits 'b' must
+  be sufficient to fit the number."
+  ([^long l]
+     (Long/toBinaryString l))
+  ([^long l ^long b]
+     (let [bits (long->bitstring l)
+           prefix-bit-count (- b (count bits))]
+       (assert (>= prefix-bit-count 0) "Number of bits must be enough to contain the value.")
+       (str (apply str (take prefix-bit-count (repeat "0")))
+            bits))))
 
 (defn long->bit-count
   "The number of bits required to represent the long value."
@@ -97,6 +142,26 @@
            (take n (repeat range-block-size))
            (concat (take remainder (repeat 1))
                    (take (- n remainder) (repeat 0)))))))
+
+(defn- split-bitstring-fn
+  "Function supports the splitting of a string into smaller strings. 'v' is a sequence
+  of one or more strings where the last string is to be split into two strings, the first
+  of length 'n'. The result returned is a sequence idential to the original sequence but
+  with the last item split into two parts."
+  [v n]
+  (let [starters (take (dec (count v)) v)
+        last (last v)
+        last-1 (subs last 0 n)
+        last-2 (subs last n)]
+    (flatten (conj '() last-2 last-1 starters))))
+
+(defn split-bitstring
+  "Split the bitstring 's' into a sequence of bitstrings of sizes defined in ranges 'r'."
+  [s r]
+  (assert (== (count s) (reduce + r)) "The total size of the sub-bitstrings must be the same as the size of the initial bitstring")
+  (assert (reduce #(and %1 (>= %2 1)) true r) "Sub-bitstrings must be of size 1 or above.")
+  (assert (not= 0 (count s)) "There must be a non-empty bitstring to split.")
+  (reduce split-bitstring-fn [s] (drop-last r)))
 
 (defn split-seq-fn
   "Function supporting the spliting of a sequence into smaller sequences. Expects 'v'
--- test/sparse/utils_test.clj
+++ test/sparse/utils_test.clj
@@ -14,6 +14,38 @@
   (testing "range must be greater than zero"
     (is (thrown? AssertionError (num->single-bit-in-seq 10 0 0)))))
 
+(deftest bit-to-set-tests
+  (testing "Correct bit to set returned (zero based)."
+    (is (= 0 (bit-to-set 1 0 1)))
+    (is (= 0 (bit-to-set 1 1 1)))
+    (is (= 0 (bit-to-set 1 0 Long/MAX_VALUE)))
+    (is (= 0 (bit-to-set 1 Long/MAX_VALUE Long/MAX_VALUE)))
+    (is (= 0 (bit-to-set 27 0 999999)))
+    (is (= 26 (bit-to-set 27 999999 999999)))
+    (is (= 6 (bit-to-set 7 137 137)))
+    (is (= 3 (bit-to-set 7 68 137)))
+    (is (= 0 (bit-to-set 7 0 137)))
+    (is (= 0 (bit-to-set 7 19 137)))
+    (is (= 1 (bit-to-set 7 20 137)))
+    (is (= 6 (bit-to-set 7 118 137)))
+    (is (= 5 (bit-to-set 7 117 137)))
+    (is (= 2 (bit-to-set 7 58 137)))
+    (is (= 0 (bit-to-set 100 0 12)))
+    (is (= 99 (bit-to-set 100 12 12)))
+    (is (= 41 (bit-to-set 100 5 12)))))
+
+(deftest bits-to-binary-string
+  (testing "Set of active bit positions turned into binary string correctly
+            (don't forget that positions are zero-based)."
+    (is (= "1" (bits->binarystring 1 #{0})))
+    (is (= "0" (bits->binarystring 1 nil)))
+    (is (= "0" (bits->binarystring 1 #{})))
+    (is (= "00000" (bits->binarystring 5 nil)))
+    (is (= "00000" (bits->binarystring 5 #{})))
+    (is (= "1010101" (bits->binarystring 7 #{0 2 4 6})))
+    (is (= "100000000000000000000" (bits->binarystring 21 #{20})))
+    (is (= "111111111111111111011" (bits->binarystring 21 #{0 1 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20})))))
+
 (deftest num-to-bit-seq-one-bit-limit
   (testing "numbers in a range always set bit 1 in a 1 bit size array. Silly test, sensible limit"
     (is (= '(1) (num->single-bit-in-seq 1 2345 2345)))
@@ -22,6 +54,10 @@
     (is (= '(1) (num->single-bit-in-seq 1 0 1)))
     (is (= '(1) (num->single-bit-in-seq 1 2345 2345)))
     (is (= '(1) (num->single-bit-in-seq 1 99998 99999)))))
+
+(deftest num-to-bit-string-one-bit-only
+  (testing "single bit value turned into length one bit string - smallest limit"
+    (is (= "1" (bits->binarystring 1 )))))
 
 (deftest num-to-bit-seq-range-same-as-number-of-bits
   (testing "numbers in range 0 to 9 into bit sequence of length 10"
