3e18bf2bba936d975f7d35795b72ad29232246db
Matt Daley <matt.daley@blueyonder.co.uk>
2014-10-27 19:55:35 +0000

    Added a script to release to clojars.

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+lein vcs assert-committed
+lein change version leiningen.release/bump-version release
+lein marg
+lein vcs commit
+git tag `cat project.clj | grep defproject | cut -d" " -f 3 | tr -d "\""`
+lein deploy clojars
+lein change version leiningen.release/bump-version
+lein vcs commit
+lein vcs push
