7903d19ae42bb71aa83780af60a6f489f1d5ca7b
Matthew Daley <matthew.daley@nokia.com>
2014-10-29 11:04:06 +0000

    Updated release.sh so that it generates the documentation automatically.

--- release.sh
+++ release.sh
@@ -3,8 +3,11 @@
 lein vcs assert-committed
 lein change version leiningen.release/bump-version release
 lein marg
+git add docs
 lein vcs commit
 git tag `cat project.clj | grep defproject | cut -d" " -f 3 | tr -d "\""`
+git push origin :gh-pages
+git subtree push --prefix docs origin gh-pages
 lein deploy clojars
 lein change version leiningen.release/bump-version
 lein vcs commit
